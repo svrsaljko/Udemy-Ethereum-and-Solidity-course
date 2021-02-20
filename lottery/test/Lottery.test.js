86f344b726c75c27533ec6afd0f2d087c532d34e
svrsaljko <vrsaljkostjepan@gmail.com>
2021-02-20 21:52:33 +0100

    Updated compile and deploy script, added test for deploying

--- lottery/test/Lottery.test.js
+++ lottery/test/Lottery.test.js
@@ -1,36 +1,24 @@
-const assert = require("assert");
-const ganache = require("ganache-cli");
-const Web3 = require("web3");
+const assert = require('assert');
+const ganache = require('ganache-cli');
+const Web3 = require('web3');
 const web3 = new Web3(ganache.provider());
+
 const {interface, bytecode} = require('../compile');
 
+let lottery;
 let accounts;
-let inbox;
-const INITIAL_STRING = 'Hi there!';
 
-beforeEach( async () => {
-  // Get a list of all accounts
-  accounts = await web3.eth.getAccounts()
-  // Use one of those accounts to deploy
-  // the contract
-  inbox = await new web3.eth.Contract(JSON.parse(interface))
-    .deploy({ data: bytecode, arguments: [INITIAL_STRING] })
+beforeEach(async ()=> {
+  accounts = await  web3.eth.getAccounts();
+
+  lottery = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({data: bytecode})
     .send({ from: accounts[0], gas: '1000000' });
-
 });
 
-describe('Inbox', () => {
-  it('deploys a contract', () => {
-     assert.ok(inbox.options.address);
+describe('Lottery Contract', () => {
+  it('deploys a contract', ()=> {
+    assert.ok(lottery.options.address);  
   })
+});
 
-  it('has a default message', async () => {
-    const message = await inbox.methods.message().call();
-    assert.equal(message,INITIAL_STRING);
-  })
-  it('can change the message', async () => {
-    await inbox.methods.setMessage(INITIAL_STRING + ' And bye!').send({ from: accounts[0] });
-    const message = await inbox.methods.message().call();
-    assert.equal(message,INITIAL_STRING + ' And bye!');
-  })
-})
